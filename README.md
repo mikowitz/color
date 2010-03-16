0b6fd6edf02d0141ca7b304a06a764e5fbe539b7
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-03-15 20:20:15 -0400

    Removing source code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# color
+
+This gem is now `Crayon` and can be found at http://github.com/mikowitz/crayon.
--- spec/term_test.rb
+++
@@ -1,8 +0,0 @@
-## EXPERIMENTATION
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'color'
-
-Color.bold_underline_blue_on_yellow("This should be formatted the same as the line below")
-Color.on_yellow_bold_blue_underline("This should be formatted the same as the line above")
